5ebb809724aad681c04c521a99796d664e80279b
k0ndrateff <63606405+k0ndrateff@users.noreply.github.com>
2020-04-22 23:04:35 +0300

    Update Controller.ino

--- Controller.ino
+++ Controller.ino
@@ -1,9 +1,9 @@
 /** 
     SPGBoat 2020
-    This file contains source code for the controller 
-    Original author: @k0ndrateff
-    Mechanical engineer: @bobr137
-    Version: 4.0 14/04/20
+    Исходный код пульта
+    Автор: @k0ndrateff
+    Инженер: @bobr137
+    Версия: 4.1 22/04/20
 */
 
 #include <LiquidCrystal_I2C.h>
@@ -12,7 +12,7 @@
 #include <nRF24L01.h>                                          
 #include <RF24.h>    
 
-// 3-D Joystick constants
+// Константы джойстика
 const int8_t Xaxis  = A1;
 const int8_t Yaxis  = A2;  
 const int8_t Button = 4;
@@ -22,14 +22,14 @@ uint16_t     YborderMIN = 505;
 uint16_t     YborderMAX = 525;
 uint16_t     Xvol = 0, Yvol = 0; 
 
-//Radio module define
+//Определение радио модуля
 RF24     radio(7, 10);
 int      data[5];
 
-//LCD define
+//Инициализация дисплея
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
-//Parameters
+//Параметры
 int spd = 0;
 int state = 0;
 int dist = 0;
@@ -38,7 +38,7 @@ bool isAuto = 0;
 void setup() {
   pinMode(Button, INPUT);
 
-  //LCD intro
+  //Экранная заставка
   lcd.init();
   lcd.backlight(); 
   lcd.print("SPGboat 2020");
@@ -51,7 +51,7 @@ void setup() {
   lcd.setCursor(0, 1);
   lcd.print("Loading...");
 
-  //Radio module init
+  //Инициализация радио модуля
   radio.begin           ();                                  
   radio.setChannel      (27);                                
   radio.setDataRate     (RF24_1MBPS);                        
@@ -62,15 +62,15 @@ void setup() {
 }
 
 void loop() {
-  // Reading joystick information
+  // Считывание положения джойстика
   Xvol = analogRead(Xaxis);
   Yvol = analogRead(Yaxis);
   state = computeState(Xvol, Yvol);
 
-  //LCD Update
+  //Обновление экрана
   lcdUpdate(state);
 
-  //Forming data package
+  //FФормирование пакета данных
   if (isAuto) {
     data[0] = 0;
   }
@@ -78,61 +78,61 @@ void loop() {
     data[0] = state;
   }
   
-  //Data broadcast
+  //Передача данных
   radio.write( &data , sizeof(data) );      
                                             
   delay(200);
 }
 
 /**
-    Computes state of the joystick and boat direction.
+    Вычисляет направление движения катера.
 */
 int computeState(Xvol, Yvol) {
   if (Xvol < XborderMIN) {                
     if (Yvol < YborderMIN) {              
-      //Reverse-left 
+      //Назад-влево 
       return 2;     
     }
     else if (Yvol > YborderMAX) {         
-      //Forward-left
+      //Вперед-влево
       return 3;         
     }
     else {
-      //Left   
+      //Влево   
       return 4;        
     }
   }
   else if (Xvol > XborderMAX) {           
     if (Yvol < YborderMIN) {              
-      //Reverse-right
+      //Назад-вправо
       return 5;      
     }
     else if (Yvol > YborderMAX) {         
-      //Forward-right
+      //Вперед-вправо
       return 6;
     }
     else {
-      //Right 
+      //Вправо 
       return 7;          
     }
   }
   else {                                  
     if (Yvol < YborderMIN) {              
-      //Reverse
+      //Назад
       return 8;            
     }
     else if (Yvol > YborderMAX) {         
-      //Forward 
+      //Вперед 
       return 9;             
     }
     else {
-      //Centre  
+      //Центр  
       return 0;        
     }
   }
   if (digitalRead(Button)) {              
     delay(1);                             
-    //Button clicked  
+    //Кнопка нажата  
     return 1;   
     while (digitalRead(Button)) {}        
     delay(10);
@@ -148,11 +148,11 @@ int computeState(Xvol, Yvol) {
 void lcdUpdate(state) {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("S:"); //Speed output
+  lcd.print("S:"); //Скорость
   lcd.setCursor(3, 0);
   lcd.print(spd);
   lcd.setCursor(0, 1);
-  lcd.print("D:"); //Distance output
+  lcd.print("D:"); //Дистанция
   lcd.setCursor(3, 1);
   lcd.print(dist);
 
